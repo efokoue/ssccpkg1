9469ea5fa7fa9794ba7bebb4607038fcf249e93e
efokoue <epfeqa@rit.edu>
2021-03-06 14:25:29 -0500

    Add files via upload

---
+++ R/descstat.R
@@ -0,0 +1,30 @@
+#' Generates and displays both graphical summaries and numerical summaries for numeric data
+#' @export
+#' @param x is the vector containing the random sample to be used
+
+
+descstat <- function(x)
+{
+
+
+  # Graphical summaries
+  par(mfrow=c(2,2))
+
+
+  # Horizontal boxplot
+  boxplot(x, horizontal = TRUE)
+
+
+  # Vertical boxplot
+  boxplot(x, horizontal = FALSE)
+
+
+  # Histogram
+  hist(x, col=rainbow(9), prob=T)
+  lines(density(x), lwd=3, col='red')
+  rug(x)
+
+
+  # QQ plot
+  qqnorm(x); qqline(x, col = 2)
+}
